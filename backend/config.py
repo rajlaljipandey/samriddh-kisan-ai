995fe3098c0e4c4b4b3de937819e5085cdf57e31
Raj Lalji Pandey <praj72498@gmail.com>
2026-02-27 21:14:29 +0530

    Switch to cloud database

--- backend/config.py
+++ backend/config.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # ---------------- DATABASE ----------------
-DATABASE_URL = "postgresql://postgres:Ballia%40123@localhost:5432/agri_ai"
+DATABASE_URL = "postgresql://samriddh_db_user:9GBh7IAZ63qGpXZTdUq3dISe9ti9qdp5@dpg-d6grldjh46gs73dqejqg-a/samriddh_db"
 
 # ---------------- MODEL PATH ----------------
 MODEL_PATH = os.path.join(BASE_DIR, "ml", "models", "crop_model.keras")
